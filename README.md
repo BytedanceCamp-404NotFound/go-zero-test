47261f88180ccb0abb9404d103a874ff40365477
yzx <ujm456@126.com>
2023-01-15 09:27:57 -0800

    add: 尝试增加新的接口

--- README.md
+++ README.md
@@ -1,18 +1,42 @@
-# test
+# go-zero-test
 测试传代码
 
 
-启动go-zero
+# 编译
 ```shell
 cd greet
 go run greet.go -f etc/greet-api.yaml
 ```
-测试go-zero
+
+# 调试
+在任何位置启动F5都可以使用，但是需要先编译
+
+
+## 创建go-zero
+```shell
+goctl api new great   # 多了一个整个great文件夹
+go mod tidy
+```
+
+## 修改API
+goctl api go -api greet.api -dir . -style go_zero
+goctl api go -api greet.api -dir . -style gozero
+
+# 启动go-zero
+```shell
+cd greet
+go run greet.go -f etc/greet-api.yaml
+```
+# 测试go-zero
 ```shell
 curl -i -X GET http://localhost:8888/from/you
 ```
+浏览器中打开
+http://[自己IP地址]:8080/from/you
 
 
 # 参考资料
 
 https://go-zero.dev/cn/docs/quick-start/monolithic-service
+https://go-zero.dev/cn/docs/goctl/goctl/
+https://go-zero.dev/cn/docs/goctl/goctl/
--- greet/internal/logic/douyin_feed_logic.go
+++ greet/internal/logic/douyin_feed_logic.go
@@ -9,22 +9,23 @@ import (
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
-type GreetLogic struct {
+type DouyinFeedLogic struct {
 	logx.Logger
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 }
 
-func NewGreetLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GreetLogic {
-	return &GreetLogic{
+func NewDouyinFeedLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DouyinFeedLogic {
+	return &DouyinFeedLogic{
 		Logger: logx.WithContext(ctx),
 		ctx:    ctx,
 		svcCtx: svcCtx,
 	}
 }
 
-func (l *GreetLogic) Greet(req *types.Request) (resp *types.Response, err error) {
+func (l *DouyinFeedLogic) DouyinFeed(req *types.Request) (resp *types.Response, err error) {
 	// todo: add your logic here and delete this line
-
+	resp = new(types.Response)
+	resp.Message = "DouyinFeed"
 	return
 }
